58dafd51ff2d0ccda0fdb9fb57cee861082de546
matthieupoyade <m.poyade@gsa.ac.uk>
2015-06-17 13:42:08 +0100

    Revert "Revert "Add""
    This reverts commit 393a771440e15631621938fdb3532833fd95557b.

---
+++ Unity/AHRC Transforming Transformation/3DSoundProject/Assets/Script/PlayOnMouse.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using TBE_3DCore;
+
+public class PlayOnMouse : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+         transform.GetComponent<TBE_Source>().Play();
+    }
+}
