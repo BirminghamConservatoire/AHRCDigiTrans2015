2a3f04a99e88b567af22b734d1f110b8df6c86aa
matthieupoyade <m.poyade@gsa.ac.uk>
2015-06-22 10:40:07 +0100

    Scene 2. Interaction with Interface Mouse
    Development Of the Interface designed by Jamie - On going

---
+++ Unity/AHRC Transforming Transformation/3DSoundProject/Assets/Script/Load3DSoundsInUI.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TBE_3DCore;
+
+
+//Load the sounds in th GUI interface
+
+public class Load3DSoundsInUI : MonoBehaviour {
+
+    public Transform soundIcon;
+    private AudioClip[] myAudioClips;
+
+    public GameObject mainGraphicPanel;
+
+    private GameObject Sphere; 
+    
+    
+    void Awake()
+    {
+        LoadAllSoundsFromResources();
+    }
+
+
+    void LoadAllSoundsFromResources()
+    {
+        int row = 0;
+
+        //Load all the clips from the Resources\Clips
+        myAudioClips = Resources.LoadAll<AudioClip>("Clips");
+
+        //For each clip
+        for (int i = 0; i < myAudioClips.Length; i++)
+        {
+            //Instantiate the prefab
+            Transform iconObj = Instantiate(soundIcon);
+
+            //Set it up in the hierarchy
+            iconObj.SetParent(transform);
+
+            //Name the IconObj
+            iconObj.name = myAudioClips[i].name;
+
+            //Set the Icon text
+            iconObj.GetChild(0).GetComponent<Text>().text = myAudioClips[i].name;
+
+            //Activate IconObj
+            iconObj.gameObject.SetActive(true);
+
+            /*********************************************************/
+            //Set RectTransform
+            //we maximize the number of icon per row to 2
+            if (i % 2 == 0)
+                row++;
+
+            float yOffset = iconObj.parent.GetComponent<RectTransform>().sizeDelta.y / 15;
+            //Position
+            iconObj.GetComponent<RectTransform>().localPosition = new Vector3(iconObj.parent.GetComponent<RectTransform>().sizeDelta.x / 4 * (1 + 2 * (i % 2)), - iconObj.parent.GetComponent<RectTransform>().sizeDelta.y/8 * (row) + yOffset, 0.0f);
+
+            //Scale
+            iconObj.GetComponent<RectTransform>().localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            /*********************************************************/
+
+            //Assign the ArudioSourceComponent to the iconObj.
+            //iconObj.gameObject.AddComponent<TBE_Source>();
+            //iconObj.GetComponent<TBE_Source>().clip = myAudioClips[i];
+        }       
+    }
+
+    void Update()
+    {
+        //Debug.Log((Screen.width / mainGraphicPanel.transform.parent.GetComponent<RectTransform>().sizeDelta.x));
+    }
+
+    public void DragIconObjToScene(GameObject obj)
+    {
+        //Debug.Log("Drag Icon Object");
+
+        //On Drag Object
+        GameObject.Find(obj.name + "bis").transform.position = Input.mousePosition;
+
+
+        if (mainGraphicPanel.GetComponent<RectTransform>().rect.Contains(GameObject.Find(obj.name + "bis").transform.position))
+        {
+            //Debug.Log("Icon Dragged in Main Graphic Panel");
+
+            //Projection from Screen to Ortho camera - p return a coordinates float values for x and y between 0 and 1
+            /*Vector2 p = GameObject.Find("Main Camera").GetComponent<Camera>().ScreenToViewportPoint(new Vector3(
+                                                                                                   Input.mousePosition.x,
+                                                                                                   Input.mousePosition.y,
+                                                                                                   Input.mousePosition.z));*/
+
+            //Projection from RT to Viewport - p return a coordinates float values for x and y between 0 and 1
+            //Debug.Log("x " + (mainGraphicPanel.transform.parent.GetComponent<RectTransform>().sizeDelta.x/Screen.width) * (Input.mousePosition.x - mainGraphicPanel.GetComponent<RectTransform>().position.x) / mainGraphicPanel.GetComponent<RectTransform>().sizeDelta.x);
+            //Debug.Log("y " + (mainGraphicPanel.transform.parent.GetComponent<RectTransform>().sizeDelta.y/Screen.height) * (Input.mousePosition.y - mainGraphicPanel.GetComponent<RectTransform>().position.y) / mainGraphicPanel.GetComponent<RectTransform>().sizeDelta.y);
+            Vector2 p = new Vector2((mainGraphicPanel.transform.parent.GetComponent<RectTransform>().sizeDelta.x / Screen.width) * (Input.mousePosition.x - mainGraphicPanel.GetComponent<RectTransform>().position.x) / mainGraphicPanel.GetComponent<RectTransform>().sizeDelta.x,
+                                    (mainGraphicPanel.transform.parent.GetComponent<RectTransform>().sizeDelta.y / Screen.height) * (Input.mousePosition.y - mainGraphicPanel.GetComponent<RectTransform>().position.y) / mainGraphicPanel.GetComponent<RectTransform>().sizeDelta.y);
+            //Clamp the value of p
+            p.x = Mathf.Clamp(p.x, 0.0f, 1.0f);
+            p.y = Mathf.Clamp(p.y, 0.0f, 1.0f);
+
+
+            //Projection from Ortho Camera (Viewport) to Perspective Camera
+            Ray rayPerspective = GameObject.Find("Scene Camera").GetComponent<Camera>().ViewportPointToRay(new Vector3(p.x, p.y, 0.0f));
+            Debug.DrawRay(rayPerspective.origin, rayPerspective.direction * 10, Color.green);
+
+            RaycastHit hit;
+            if (Physics.Raycast(rayPerspective, out hit, 100))
+                 Sphere.transform.position = rayPerspective.origin + rayPerspective.direction * hit.distance/2;
+
+        }
+
+        
+    }
+
+    public void OnDownObj(GameObject obj)
+    {
+        //Debug.Log("Down Icon Object");
+
+        //Duplicate the Icon Object
+        GameObject go = Instantiate<GameObject>(obj);
+        go.name = obj.name + "bis";
+        go.transform.SetParent(obj.transform.parent);
+        //Set degree of transparency
+        go.GetComponent<RawImage>().color = new Color(go.GetComponent<RawImage>().color.r, go.GetComponent<RawImage>().color.g, go.GetComponent<RawImage>().color.b, 0.25f);
+
+        //Create Sphere
+        Sphere = Instantiate<GameObject>(GameObject.Find("Sphere"));
+        Sphere.name = "Sphere" + obj.name;
+    }
+
+    public void OnUpObj(GameObject obj)
+    {
+        //Debug.Log("Up Icon Object");
+
+        //Delete the Icon Obj
+        Destroy(GameObject.Find(obj.name + "bis"));
+    }
+}
---
+++ Unity/AHRC Transforming Transformation/3DSoundProject/Assets/Script/SetSceneCameraView.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//We set the projection matrix of the camera scene so that the edges of the room are fitting into the screen
+
+public class SetSceneCameraView : MonoBehaviour {
+
+    Camera mySceneCamera;
+
+    private float left;
+    private float right;
+    private float top;
+    private float bottom;
+
+
+	// Use this for initialization
+	void Start () {
+        
+        //Get the Scene Camera
+        mySceneCamera = transform.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        //Set the Nearclip of the Scene Camera to the edge of the floor object
+        mySceneCamera.nearClipPlane = -1.0f * (transform.localPosition.z + (GameObject.Find("Floor").transform.localScale.z * 10 / 2));
+
+        //Set the Field of View angle of the Scen Camera
+        //mySceneCamera.fieldOfView = Mathf.Atan((GameObject.Find("Floor").transform.localScale.x * 10 / 2) / mySceneCamera.nearClipPlane) * Mathf.Rad2Deg;
+
+        //Set Matrix Parameters
+        left = -1 * (GameObject.Find("Floor").transform.localScale.x * 10 / 2);
+        right = GameObject.Find("Floor").transform.localScale.x * 10 / 2;
+        bottom = -1 * (GameObject.Find("Back").transform.localScale.z * 10 / 2);
+        top = GameObject.Find("Back").transform.localScale.z * 10 / 2;  
+
+        //Set the frustum on th Near Clip plane of the scene camera to the edge of the Interaction Volume - We just readjust the matrix
+         Matrix4x4 m  = RefineProjectionMatrix(left, right, bottom, top, mySceneCamera.nearClipPlane, mySceneCamera.farClipPlane);
+         mySceneCamera.projectionMatrix = m;
+	}
+
+    static Matrix4x4 RefineProjectionMatrix(float left, float right, float bottom, float top, float near, float far)
+    {
+        float x = 2.0F * near / (right - left);
+        float y = 2.0F * near / (top - bottom);
+        float a = (right + left) / (right - left);
+        float b = (top + bottom) / (top - bottom);
+        float c = -(far + near) / (far - near);
+        float d = -(2.0F * far * near) / (far - near);
+        float e = -1.0F;
+        Matrix4x4 m = new Matrix4x4();
+        m[0, 0] = x;
+        m[0, 1] = 0;
+        m[0, 2] = a;
+        m[0, 3] = 0;
+        m[1, 0] = 0;
+        m[1, 1] = y;
+        m[1, 2] = b;
+        m[1, 3] = 0;
+        m[2, 0] = 0;
+        m[2, 1] = 0;
+        m[2, 2] = c;
+        m[2, 3] = d;
+        m[3, 0] = 0;
+        m[3, 1] = 0;
+        m[3, 2] = e;
+        m[3, 3] = 0;
+        return m;
+    }
+}
