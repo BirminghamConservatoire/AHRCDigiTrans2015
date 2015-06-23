23fc11a12760f5cff5f8add5b508a3193c0d7efe
Jamie Bullock <jamie@jamiebullock.com>
2015-06-23 17:14:12 +0100

    Added 3DCeption splash screen
    - Adding this is a requirement of the 3Dception free / non-commercial license

---
+++ Unity/AHRC Transforming Transformation/3DSoundProject/Assets/Scene/Splash/DismissSplash.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DismissSplash : MonoBehaviour {
+	public float delayTime = 2;
+	IEnumerator Start () { 
+		yield return new WaitForSeconds( delayTime );
+		Application.LoadLevel( 1 );
+	}
+}
